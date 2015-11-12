83b5cb06023cc391c548e0f59c5b7235c1d93c5e
Pedro Romano <pedro.romano@holidayextras.com>
2015-11-12 15:16:52 +0000

    Add unit test for temporarily unavailable resource.

---
+++ test/503.js
@@ -0,0 +1,42 @@
+"use strict";
+var request = require("request");
+var assert = require("assert");
+var jsonApi = require("../lib/jsonApi");
+var jsonApiTestServer = require("../example/server");
+
+
+describe("Testing jsonapi-server", function() {
+
+  describe("resource readiness", function() {
+
+    it("returns 200 if resource is ready", function(done) {
+      var url = "http://localhost:16006/rest/articles/de305d54-75b4-431b-adb2-eb6b9e546014";
+      request.get(url, function(err, res) {
+        assert(!err);
+        assert.strictEqual(res.statusCode, 200, "Expecting 200 OK");
+        done();
+      });
+    });
+
+    it("returns 503 if resource is NOT ready", function(done) {
+      var handlers = jsonApi._resources.articles.handlers;
+      var savedHandlersReady = handlers.ready;
+      handlers.ready = false;
+      var url = "http://localhost:16006/rest/articles/de305d54-75b4-431b-adb2-eb6b9e546014";
+      request.get(url, function(err, res) {
+        assert(!err);
+        assert.strictEqual(res.statusCode, 503, "Expecting 503 SERVICE UNAVAILABLE");
+        handlers.ready = savedHandlersReady;
+        done();
+      });
+    });
+
+  });
+
+  before(function() {
+    jsonApiTestServer.start();
+  });
+  after(function() {
+    jsonApiTestServer.close();
+  });
+});
