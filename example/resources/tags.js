dcbdd2e8f84ab877602108612827fcfacae78581
hxoliverrumbelow <oliver.rumbelow@holidayextras.com>
2015-12-10 11:02:20 +0000

    Adding a complex recursive relationship

--- example/resources/tags.js
+++ example/resources/tags.js
@@ -13,23 +13,43 @@ jsonApi.define({
     articles: jsonApi.Joi.belongsToMany({
       resource: "articles",
       as: "tags"
+    }),
+    parent: jsonApi.Joi.one("tags"),
+    children: jsonApi.Joi.belongsToMany({
+      resource: "tags",
+      as: "parent"
     })
   },
   examples: [
     {
       id: "7541a4de-4986-4597-81b9-cf31b6762486",
       type: "tags",
-      name: "live"
+      name: "live",
+      parent: { type: "tags", id: "2a3bdea4-a889-480d-b886-104498c86f69" }
     },
     {
       id: "2a3bdea4-a889-480d-b886-104498c86f69",
       type: "tags",
-      name: "staging"
+      name: "staging",
+      parent: { type: "tags", id: "6ec62f6d-9f82-40c5-b4f4-279ed1765492" }
     },
     {
       id: "6ec62f6d-9f82-40c5-b4f4-279ed1765492",
       type: "tags",
-      name: "needs-work"
+      name: "building",
+      parent: { type: "tags", id: "68538177-7a62-4752-bc4e-8f971d253b42" }
+    },
+    {
+      id: "68538177-7a62-4752-bc4e-8f971d253b42",
+      type: "tags",
+      name: "development",
+      parent: { type: "tags", id: "8d196606-134c-4504-a93a-0d372f78d6c5" }
+    },
+    {
+      id: "8d196606-134c-4504-a93a-0d372f78d6c5",
+      type: "tags",
+      name: "planning",
+      parent: null
     }
   ]
 });
